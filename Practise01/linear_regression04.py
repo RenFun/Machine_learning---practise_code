73a5cb068f8b4fa43dee487b2d9cd79179330a00
Renfun <1849148621@qq.com>
2021-09-21 17:14:58 +0800

    9.21

--- Practise01/linear_regression04.py
+++ Practise01/linear_regression04.py
@@ -3,32 +3,60 @@
 # Time: 2021/06/05
 
 
-# 导入sklearn库实现线性回归
-
+# 导入sklearn库实现一元线性回归
 import numpy as np
 import matplotlib.pyplot as plt
-
-points = np.genfromtxt('data.csv', delimiter=',')
-x = points[:, 0]                # 第一列数据
-y = points[:, 1]                # 第二列数据
-num = len(points)               # len()函数求出列表points有多少个元素；count（）函数求出列表中指定元素出现的次数
-print(num)                      # 共100个点
-plt.scatter(x, y)
-
-# 导入sklearn库
 from sklearn.linear_model import LinearRegression
-lr = LinearRegression()
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+
+# 加载数据集
+points = np.genfromtxt('data.csv', delimiter=',')
+x = points[:, 0]                  # 第一列数据
+y = points[:, 1]                  # 第二列数据
 # 数组新的shape属性应该要与原来的一致，如果等于-1的话，那么Numpy会根据剩下的维度计算出数组的另外一个shape属性值。
-x_new = x.reshape(-1, 1)            # 此时是将x变成列数为1的数组，若是x.reshape(1, -1)表示将x变成行数为1的数组
-y_new = y.reshape(-1, 1)            # 变成一列
-print(x_new, y_new)
-lr.fit(x_new, y_new)                # 拟合线性模型，两个参数X，y。X是训练数据集，矩阵或者数组形式，y是标记
-# LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False)
+x = x.reshape(-1, 1)              # 此时是将x变成列数为1的数组，若是x.reshape(1, -1)表示将x变成行数为1的数组
+y = y.reshape(-1, 1)              # 变成一列
 
-print(lr.coef_)                 # LinearRegression()函数的属性：线性模型的系数，多元线性回归模型时是（w1,w2,...wm）形式
-print(lr.intercept_)            # LinearRegression()函数的属性：线性模型的截距
+# 将数据集划分为训练集和测试集，训练集比例为0.7
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7)
 
-f = lr.predict(x_new)           # f为预测值，调用LinearRegression().predict(x)可以得到预测值
-# f = x * lr.coef_[0] + lr.intercept_[0]
-plt.plot(x, f, 'r')
+# 定义线性回归模型
+lr = LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=None)
+
+# 拟合线性模型，两个参数X，y，X是训练数据集，矩阵或者数组形式，y是标记
+lr.fit(x_train, y_train)
+# lr.coef_是系数，lr.intercept_是截距
+print('系数：', lr.coef_)
+print('截距：', lr.intercept_)
+# y_predict为通过拟合模型进行预测得到的预测值
+y_predict = lr.predict(x_test)
+
+# 绘制图像1
+plt.rcParams['font.sans-serif'] = ['SimHei']     # 用来正常显示中文，设置字体为黑体
+plt.title("训练集散列图和拟合的模型")
+plt.xlabel("训练集的x坐标")
+plt.ylabel("训练集的y坐标")
+# 绘制训练集散列图
+plt.scatter(x_train, y_train)
+# plt.show()
+# 绘制拟合的线性模型曲线
+plt.plot(x_train, lr.coef_ * x_train + lr.intercept_, 'r')
 plt.show()
+
+# 绘制图像2
+plt.title("预测值与真实值的关系")
+plt.xlabel("测试集样本")
+plt.ylabel("预测值和真实值")
+# 生成一个序列，1-30，步长是1
+t = np.arange(1, len(x_test)+1, 1)
+plt.plot(t, y_test, color='blue', label="真实值")
+plt.plot(t, y_predict, color='red', label="预测值")
+plt.legend(loc='upper left')
+plt.show()
+
+# 模型的性能指标：均方误差，决定系数
+print('均方误差: %.2f' % mean_squared_error(y_test, y_predict))
+print('决定系数：%.2f' % r2_score(y_test, y_predict))
--- Practise01/linear_regression05.py
+++ Practise01/linear_regression05.py
@@ -3,42 +3,67 @@
 # Time: 2021/09/14
 
 
-# 调用sklearn库中的函数
+# 调用sklearn库实现多元线性回归
 # 使用UCI官方数据集或者sklearn库中的数据集（糖尿病病人）
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn import datasets, linear_model
+from sklearn import datasets
+from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 
-# 加载糖尿病数据集：总共有442个样本，每个样本有10个属性描述
-diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)                # 返回（data，target）形式数据
-print(diabetes_X)
-# 将442个样本按属性分组，变成442*1*10，即442个样本按照一个属性分成一组，一共10组，使用10个属性中的第3个属性（编号为2）
-diabetes_X = diabetes_X[:, np.newaxis, 2]
-print(diabetes_X)
-# 利用列表的切片操作将数据集划分为训练集和数据集：编号0-421为训练集，422-441为测试集
-diabetes_X_train = diabetes_X[:-20]                     # 从列表起始到-20切片，从左至右为正方向，编号为0到441
-diabetes_X_test = diabetes_X[-20:]                      # 从-20到列表末尾切片，从右至左为负方向，编号为-1到-442
-diabetes_y_train = diabetes_y[:-20]                     # 切片操作是左闭右开，右值在正方向时-1，负方向时+1，可确定为后一个值
-diabetes_y_test = diabetes_y[-20:]
+# 加载糖尿病数据集：总共有442个样本，每个样本diabetes_X有10个属性描述，分别为：年龄、性别、体质指数、血压、s1~s6（6种血清的化验数据）
+# 但需要注意的，以上的数据是经过预处理， 10个特征都做了归一化处理。
+# diabetes_y是目标值，一年疾后的病情定量测量，它是一个连续的实数值，符合线性回归模型评估的范畴。
+diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+# 将数据集划分为训练集和测试集，训练集占0.7，随机数种子 = 0 或 none，表示重复实验时获得的随机数将不同
+diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes_y, train_size=0.7)
+lr = linear_model.LinearRegression(fit_intercept=True, normalize=False, copy_X=True, n_jobs=None)
+# 使用训练集去拟合线性模型
+lr.fit(diabetes_X_train, diabetes_y_train)
 
-lr = linear_model.LinearRegression()
-lr.fit(diabetes_X_train, diabetes_y_train)              # 使用训练集去拟合线性模型
-diabetes_y_pred = lr.predict(diabetes_X_test)           # diabets_y_pred 是预测值
+# diabetes_y_pred 是测试集的预测值
+diabetes_y_pred = lr.predict(diabetes_X_test)
+# diabetes_y_train_pred 时训练集的预测值
+diabetes_y_train_pred = lr.predict(diabetes_X_train)
 print('系数: ', lr.coef_)
 print('截距: ', lr.intercept_)
 
-# mean_squared_error()表示测试集的实际值与预测值的均方方差，即损失函数，越接近0说明模型越好
-print('均方误差: %.2f' % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-
-plt.scatter(diabetes_X_test, diabetes_y_test,  color='black')
-plt.plot(diabetes_X_test, diabetes_y_pred, color='blue', linewidth=3)
-# plt.xticks(())                    # Passing an empty list removes all xticks 传递一个空列表将会删除所有的标记
-# plt.yticks(())
+# 绘制图像1
+plt.rcParams['font.sans-serif'] = ['SimHei']        # 用来正常显示中文，设置字体为黑体
+plt.rcParams['axes.unicode_minus'] = False            # 用来正常显示负号
+plt.title("预测值与真实值之间的关系", fontsize=20)
+plt.xlabel("测试集样本")
+plt.ylabel("预测值和真实值")
+t = np.arange(1, len(diabetes_X_test)+1, 1)
+plt.plot(t, diabetes_y_test, color='red', label='真实值')
+plt.plot(t, diabetes_y_pred, color='blue', label='预测值')
+# 显示图例，设置图例的位置
+plt.legend(loc='upper left')
 plt.show()
 
+# 绘制图像2
+# 残差值图：通过将预测结果减去对应的目标变量的真实值，便可获得残差值。X轴表示预测结果，Y轴表示残差值。其中一条直线 Y=0，表示残差为0的位置
+plt.title("残差值图")
+plt.xlabel('预测值')
+plt.ylabel('残差值')
+# 训练集数据以圆形，蓝色形式散列；测试集数据以正方形，绿色形式散列
+plt.scatter(diabetes_y_train_pred, diabetes_y_train_pred - diabetes_y_train, c='blue', marker='o', label='训练集数据')
+plt.scatter(diabetes_y_pred, diabetes_y_pred - diabetes_y_test, c='green', marker='s', label='测试集数据')
+plt.legend(loc='upper left')
+# y = 0 ，表示残差值为0的位置
+plt.hlines(y=0, xmin=0, xmax=300, linestyles='solid', colors='red')
+plt.show()
+
+# 模型的性能指标：均方误差，均方根误差，平均绝对误差，决定系数
+# mean_squared_error()表示测试集的实际值与预测值的均方方差，即损失函数，越接近0说明模型越好
+# r2_score()表示相关指数，也叫决定系数，其值越接近1说明模型越好
+print('均方误差: %.2f' % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+print('均方根误差：%.2f' % np.sqrt(mean_squared_error(diabetes_y_test, diabetes_y_pred)))
+print('平均绝对误差：%.2f' % mean_absolute_error(diabetes_y_test, diabetes_y_pred))
+print('决定系数：%.2f' % r2_score(diabetes_y_test, diabetes_y_pred))
 
 # 补充知识点
 # 1.列表的切片：格式：  list[start : end : step]
